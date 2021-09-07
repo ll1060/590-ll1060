ee6ad2c0e8519000c6576fd52a6d5eaafd067f05
Leyao Li <ll1060@georgetown.edu>
2021-09-06 21:03:57 -0400

    finished hw1

--- HW1.1/RUN_ALL.sh
+++ HW1.1/RUN_ALL.sh
@@ -1,7 +1,7 @@
 ScriptLoc=${PWD}
 cd LectureCodes
-for i in *.py; do echo $i; python $i; done #run all python scripts in directory•
-grep “I HAVE WORKED” *
+for i in *.py; do echo $i; python $i; done #run all python scripts in directory
+grep "I HAVE WORKED" *
 #NOTE: grep searches through files for matching strings (very useful command)
 cd $ScriptLoc #return to script directory
 for i in *.py; do echo $i; python $i; done #run all python scripts in directory
